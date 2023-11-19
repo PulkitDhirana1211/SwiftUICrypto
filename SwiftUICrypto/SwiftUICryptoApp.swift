a79154080c018b54f068ddf23dddb2e12f0c5796
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2023-11-20 01:48:50 +0530

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by Pulkit Dhirana on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
